cb41f729f49b3e777b73009a47a4d21a0a4263a4
BananaKlit <yhylhl.10@gmail.com>
2024-12-13 11:16:56 +0100

    deployment attempt

--- src/Components/App.js
+++ src/Components/App.js
@@ -17,8 +17,8 @@ function App() {
         
       } else {
         // Redirect to the authentication flow if not authenticated
-        window.location.href = 'https://alphaapi-657550777490.us-central1.run.app/Api/Auth/Auth';
-      }
+        window.location.href = 'https://apialpha-657550777490.us-central1.run.app/Api/Auth/Auth';
+      }             
     };
 
     // Check authentication status on component mount
